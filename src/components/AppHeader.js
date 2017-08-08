565b0b298998c3bf9fd0e5ab421b3e15803d0467
Mandy <mandy@mapzen.com>
2017-08-08 10:26:22 -0400

    Go back to local spots when yelpzen header clicked

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Header } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 import FindSearchBar from './Searchbars/FindSearchBar'
 import NearSearchBar from './Searchbars/NearSearchBar'
  
 const AppHeader = (props) => {
 	return (
 		<div className={props.className}>
-			<Header as='h1'> Yelpzen </Header>
+			<Link to='/'>
+				<Header as='h1'> Yelpzen </Header>
+			</Link>
 			<div className='search-bars'>
 				<FindSearchBar className='Find' config={props.config} />
 				<NearSearchBar className='Near' config={props.config} />
